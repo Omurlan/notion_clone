0fc3270bb9315f179f0e06ef7fe8a5093d0116df
Omurlan <abdrashevomurlan@gmail.com>
2023-12-26 12:00:14 +0600

    toolbar component

--- convex/documents.ts
+++ convex/documents.ts
@@ -312,3 +312,30 @@ export const update = mutation({
     return updatedDocument;
   },
 });
+
+export const removeIcon = mutation({
+  args: { id: v.id('documents') },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error('Not authenticated');
+    }
+
+    const userId = identity.subject;
+
+    const document = await ctx.db.get(args.id);
+
+    if (!document) {
+      throw new Error('Not found');
+    }
+
+    if (document.userId !== userId) {
+      throw new Error('Unauthorized');
+    }
+
+    const updatedDocument = await ctx.db.patch(args.id, { icon: undefined });
+
+    return updatedDocument;
+  },
+});
